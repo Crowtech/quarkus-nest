df6d5df400d8caa792714f15c376213c4c4b980f
Adam Crow <adamcrow63@gmail.com>
2021-01-07 00:06:39 +1100

    Updated

--- runtime/src/main/java/au/com/crowtech/quarkus/nest/endpoints/ExampleResource.java
+++ runtime/src/main/java/au/com/crowtech/quarkus/nest/endpoints/ExampleResource.java
@@ -1,6 +1,7 @@
 package au.com.crowtech.quarkus.nest.endpoints;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,7 +9,9 @@ import javax.ws.rs.core.MediaType;
 
 import au.com.crowtech.quarkus.nest.beans.Echo;
 
-@Path("/hello")
+@Path("/v1/api/nest")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ExampleResource {
 
     @Inject
